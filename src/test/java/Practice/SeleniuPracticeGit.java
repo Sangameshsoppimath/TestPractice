69dac2a7984f7f9e104db2459647cba3a77a7b92
Sangameshsoppimath <spsangam8747@gmail.com>
2024-02-25 20:26:58 +0530

    Gitx added code

---
+++ src/test/java/Practice/SeleniuPracticeGit.java
@@ -0,0 +1,16 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class SeleniuPracticeGit 
+{
+	@Test
+	public void demo()
+	{
+		System.out.println("Test added 1");
+		System.out.println("Test added 2");
+		
+		
+	}
+
+}
